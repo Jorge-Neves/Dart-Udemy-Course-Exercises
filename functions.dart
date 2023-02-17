1fde969dcdf0299c06bc7563cadf25d7f2d09bbc
Paulo Santos <paulo.jorge.ngs@gmail.com>
2023-02-17 12:38:48 +0000

    add functions.dart

---
+++ functions.dart
@@ -0,0 +1,12 @@
+void addNumbers(int num1, int num2) {
+  print(num1 + num2);
+}
+
+int subtractNumbers(int num1, int num2) {
+  return (num1 - num2);
+}
+
+void main() {
+  addNumbers(3, 4);
+  print(subtractNumbers(3, 4));
+}
