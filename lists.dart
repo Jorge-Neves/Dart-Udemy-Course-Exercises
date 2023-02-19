1cdd5ef434d0b11e5bbb8728bf48f73523afef97
Paulo Santos <paulo.jorge.ngs@gmail.com>
2023-02-19 23:51:26 +0000

    Create lists.dart

---
+++ lists.dart
@@ -0,0 +1,14 @@
+var listOfNames = ["Paulo", "Jorge", "Nascimento", "Gomes", "Santos"];
+
+void main() {
+  print(listOfNames.elementAt(0));
+  print(listOfNames.elementAt(1));
+  print(listOfNames.elementAt(2));
+  print(listOfNames.elementAt(3));
+  print(listOfNames.elementAt(4));
+  print(listOfNames[0]);
+  print(listOfNames[1]);
+  print(listOfNames[2]);
+  print(listOfNames[3]);
+  print(listOfNames[4]);
+}
